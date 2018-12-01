d4e152f28d087b848f0e315253079eda1b860024
flyingrub <flyingrub@protonmail.com>
2018-12-01 15:19:06 +0100

    Result

--- jpeg/tp.cpp
+++ jpeg/tp.cpp
@@ -5,6 +5,7 @@
 
 #include <iostream>
 #include <fstream>
+#include <sstream>
 
 void display_plot(const char* filename, bool color);
 
@@ -19,11 +20,17 @@ int main(int argc, char **argv)
 
 	ImageBase imIn, im2;
 	imIn.load("../perroquet.ppm");
-	int quantize_quality = 90;
-	auto data = imIn.fullCompress(quantize_quality);
-	auto im = ImageBase::fromBlock(data, imIn.getWidth(), imIn.getHeight(), imIn.getColor(), quantize_quality);	// auto psnr = imIn.psnr(*im);
-	im->save("test.ppm");
-	cout << "psnr:" << im->psnr(imIn) << endl;
+	for (int i= 1; i<= 10; i++) {
+		int quantize_quality = i*10;
+		auto data = imIn.fullCompress(quantize_quality);
+		auto im = ImageBase::fromBlock(data, imIn.getWidth(), imIn.getHeight(), imIn.getColor(), quantize_quality);	// auto psnr = imIn.psnr(*im);
+		std::ostringstream stringStream;
+  		stringStream << "Test" << quantize_quality << ".ppm";
+		char* filename =(char*) stringStream.str().c_str();
+		im->save(filename);
+		cout << quantize_quality << " | psnr:" << im->psnr(imIn) << endl;
+	}
+
 
 	return 0;
 }
